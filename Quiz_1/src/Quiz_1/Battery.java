f8ddcb97107fec09ea127759b461e7c5b49070d4
HP USER <HP USER@LAPTOP-KGQ88H7D>
2021-04-15 14:04:41 +0800

    Quiz_1

--- Quiz_1/src/Quiz_1/Battery.java
+++ Quiz_1/src/Quiz_1/Battery.java
@@ -4,16 +4,14 @@ import java.util.*;
 
 public class Battery {
 	
-	Scanner in = new Scanner(System.in);
-	
-		
 		String brand;
 		String shape; 
 		String size;
 		double voltage;
 		
+		Scanner in = new Scanner(System.in);
 	
-	public static void setBrand(String brand) {
+	public void setBrand(String brand) {
 		
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter Brand: ");
@@ -21,20 +19,20 @@ public class Battery {
 		
 	}
 	
-	public static void setShape(String shape) {
+	public void setShape(String shape) {
 		
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter Shape: ");
 		this.shape = in.next();
 	}
 	
-	public static void setSize(String size) {
+	public void setSize(String size) {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter Size: ");
 		this.size = in.next();
 	}
 	
-	public static void setVoltage(double voltage) {
+	public void setVoltage(double voltage) {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter Voltage: ");
 		this.voltage = in.nextDouble();
